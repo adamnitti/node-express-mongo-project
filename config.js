c2320cc7e0099b2b002da9fbf24bd41e730891fd
RenaissanceMan <75748078+adamnitti@users.noreply.github.com>
2021-04-28 22:12:31 -0500

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
-    'facebook' : {
-        clientId: '5760079704065054',
-        clientSecret: 'c094c5be12c714108561aa859d8e9ffd'
-    }
-}
